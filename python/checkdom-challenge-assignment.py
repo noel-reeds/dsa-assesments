80669ef0477f2043f33f48bae23ebab90cbf95e0
altoids <noelreeds01@gmail.com>
2026-09-18 16:32:29 +0300

    checkdom

--- python/checkdom-challenge-assignment.py
+++ python/checkdom-challenge-assignment.py
@@ -1,4 +1,4 @@
-class checkdom_challenge_assignment():
+class checkdom_challenge_soln:
     def check_dom(str_param: str) -> bool or str:
         """
         Check if an HMTL tag is valid or not.
@@ -8,7 +8,7 @@ class checkdom_challenge_assignment():
         
         Return:
             bool or str: Return False if HTML tags are mismatched or invalid
-                        Retturns mismatched tag if there's only one.
+                        Returns mismatched tag if there's only one.
                         Returns True if all tags are matched and valid.
         Examples:
             >>> check_dom("<p>Hello World!</p>")
@@ -22,5 +22,12 @@ class checkdom_challenge_assignment():
 
         HTML Tags to be used:
             The elements that will be used are: <b>, <i>, <em>, <div>, <p>
+        """
+        tags = ('<b>', '<div>', '<p>')
 
-        
+        # check for existing html tags
+        # and find its corr. tag if exists.
+        for tag in tags:
+            if tag in str_param and f'</{tag}>' in str_param:
+                return True
+            return False
